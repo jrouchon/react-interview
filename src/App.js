ddedf09243abf3556aaaf6f71a4f3158ed1b75ae
jrouchon <justinerobin24@hotmail.fr>
2023-03-06 02:06:08 +0100

    doing category need filtered render and pagination

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setMoviesData } from './features/moviesSlice';
 import MovieCard from "./components/movieCard";
 
+import Select from 'react-select';
+
 function App() {
   //const [moviesData, setMoviesData] = useState([]);
   const dispatch = useDispatch()
@@ -12,14 +14,41 @@ function App() {
 
   const fetchMovies = useCallback(async() => {
     const moviesList = await movies$.then(response => response);
-    console.log(moviesList);
-    //setMoviesData(moviesList);
+    console.log(moviesList); //a enlever
     dispatch(setMoviesData(moviesList));
 }, [dispatch]);
 
   useEffect(() => {
     fetchMovies();
 }, [fetchMovies])
+
+  function categoriesList(moviesData) {
+    const categoriesOption = []
+    const categoriesList = moviesData?.map((movie) => (movie.category))
+    if(categoriesList) {
+      for ( const category of categoriesList) {
+        if (!categoriesOption.includes(category)) {
+          categoriesOption.push(category)
+        }
+      }
+    }
+    return (categoriesOption)
+  }
+
+  
+  const handleChangeCategorySelection = (selectedOption) => {
+    console.log("selected option", selectedOption);
+    // todo pagination et render du tri
+  }
+
+  const categories = categoriesList(moviesData);
+  //console.log("categories :", categories)
+  
+  const selectCategoryOptions = categories.map((category) => ({
+    label: category,
+    value: category,
+  }))
+  //console.log("selectCategoryOptions", selectCategoryOptions)
 
   return (
     <div className="App">
@@ -30,6 +59,9 @@ function App() {
         </div>
       </header>
       <main>
+        <div className="multiselect-wrapper">
+          <Select className="multiselect" options={selectCategoryOptions} onChange={handleChangeCategorySelection} placeholder={"Filtrer par genre"} isMulti/>
+        </div>
         <div className="cards-wrapper">
           {moviesData?.map((movie, index) => (
             <MovieCard key={index} movie={movie}/>
@@ -47,4 +79,14 @@ export default App;
         <div className="cards-wrapper">
           {moviesData?.map((movie, index) => (console.log(index, movie.id , "movie : ", movie.title)))}
         </div>
+
+        <Select
+          //value={selectedOptions}
+          options={categoryOption}
+          //onChange={onChange}
+          closeMenuOnSelect={false}
+          isMulti
+          name="movie category"
+          placeholder="Filtrer par catégorie"
+        />
 */
